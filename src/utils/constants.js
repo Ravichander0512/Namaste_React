116b1ebe2bdb29fc4ad4edbb150a73ff4bfed1c5
Ravichander0512 <ravichander0512@gmail.com>
2025-03-08 18:42:55 +0530

    restart

--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const RES_CARD_LOGO_BASE_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const LOGO_URL="https://www.pngall.com/wp-content/uploads/8/Cooking-Recipe.png"
