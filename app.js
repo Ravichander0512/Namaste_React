803859125a1d2b6ca6b95f23291696c7e04590a6
Ravichander0512 <ravichander0512@gmail.com>
2025-03-10 22:37:51 +0530

    episode-02 and 03

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", null, "I am h1 tag"),
-    React.createElement("h2", null, "I am h2 tag")]
-  ),React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", null, "I am h1 tag"),
-    React.createElement("h2", null, "I am h2 tag")]
-  )]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent);
+const Title = () => <h1>I am from Title component</h1>
+const HeadingComponent2 = () => <h2>I am header tag 2</h2>;
+const sum=(a,b)=>a+b;
+
+const footer=<div>
+  <h1>Footer</h1>
+  <p>Footer content</p>
+</div>
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {sum(2,3)}
+        <Title />
+        <h1>I am from header component</h1>
+        <HeadingComponent2/>
+        {footer}
+    </div>
+      
+  )
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
